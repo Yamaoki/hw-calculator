755d84a481f6b703f07db1ce1d00153a1385d5b6
Андрей <d89284146236sv@gmail.com>
2023-03-18 13:10:25 +0300

    первы комит

---
+++ src/main/java/calculator/hw/calculator/HwCalculator1Application.java
@@ -0,0 +1,13 @@
+package calculator.hw.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwCalculator1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwCalculator1Application.class, args);
+    }
+
+}
